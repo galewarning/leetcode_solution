22ff22461997aa5e2f649ff93f047679e3558cf0
galewarning <93493874+galewarning@users.noreply.github.com>
2022-03-16 16:01:59 -0400

    Time: 3 ms (92.65%), Space: 43.9 MB (38.96%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] record = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            record[c - 'a'] ++;
+        }
+        for (char c : t.toCharArray()) {
+            record[c - 'a'] --;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (record[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
