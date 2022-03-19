64575eccbb13f72d00497505cc314f50d797d651
galewarning <93493874+galewarning@users.noreply.github.com>
2022-03-19 00:27:45 -0400

    Time: 4 ms (97.57%), Space: 41.7 MB (82.15%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if ("+".equals(tokens[i])) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("-".equals(tokens[i])) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if ("*".equals(tokens[i])) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(tokens[i])) {
+                int temp1 = stack.pop();
+                int temp2 = stack.pop();
+                stack.push(temp2/temp1);
+            } else {
+                stack.push(Integer.valueOf(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
