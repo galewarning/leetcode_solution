f736f498e6f2f0cb317a827e6f97bf8717cc23ff
galewarning <93493874+galewarning@users.noreply.github.com>
2022-04-12 20:14:06 -0400

    Time: 21 ms (91.25%), Space: 42.1 MB (93.57%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if (nums.length == 0) return false;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) return false;
+        int target = sum/2;
+        int[] dp = new int[target + 1];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        return dp[target] == target;
+    }
+}
